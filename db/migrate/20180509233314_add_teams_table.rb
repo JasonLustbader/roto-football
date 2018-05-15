086c080b15405cb8443fce469fcdecb226f8abee
Jason Lustbader <Jason.Lustbader@gmail.com>
2018-05-15 17:44:36 -0500

    Add timestamps to teams

--- db/migrate/20180509233314_add_teams_table.rb
+++ db/migrate/20180509233314_add_teams_table.rb
@@ -2,6 +2,7 @@ class AddTeamsTable < ActiveRecord::Migration[5.2]
   def change
     create_table :teams do |t|
       t.string :name
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2018_05_15_223930) do
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
