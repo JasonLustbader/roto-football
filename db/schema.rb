902e76f895a15e68c664e58896800ccfdfe77f7b
Jason Lustbader <Jason.Lustbader@gmail.com>
2018-05-15 17:42:04 -0500

    Add categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_09_233314) do
+ActiveRecord::Schema.define(version: 2018_05_15_223930) do
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
